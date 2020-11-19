670536fba64c70bccc142e7c8ce7281b31dae0d9
Kartals <bkrprs@icloud.com>
2020-11-20 00:45:59 +0100

    springBoot MVC-Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -5,6 +5,8 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -17,11 +19,17 @@ public class MentorController {
     @GetMapping("/register")
     public String showForm(Model model){
 
-        model.addAttribute("mentor", new Mentor());
+        model.addAttribute("mentor", new Mentor()); //creating an empty object
 
         List<String> batchList= Arrays.asList("B7","B8","B9","B10","B11","B12","E1","E2","E3","E4");
         model.addAttribute("batchList",batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+        return "mentor/mentor-confirmation";
     }
 }
