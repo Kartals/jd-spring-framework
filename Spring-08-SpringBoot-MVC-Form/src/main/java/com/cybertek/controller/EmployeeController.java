9bfa02346097d7bd98e0a4e76376b8a22013ee41
Kartals <bkrprs@icloud.com>
2020-11-21 03:19:09 +0100

    springBoot MVC-Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -5,7 +5,12 @@ import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/employee")
@@ -20,5 +25,14 @@ public class EmployeeController {
         return "employee/employee-create";
     }
 
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+
+        model.addAttribute("employeeList", Arrays.asList(employee));
+
+        int birthYear= LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age",LocalDate.now().getYear()-birthYear);
+        return "employee/employee-list";
+    }
 
 }
