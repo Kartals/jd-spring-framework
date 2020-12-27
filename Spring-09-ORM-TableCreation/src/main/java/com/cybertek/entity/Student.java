b79a92da494d1d5468e99f31914c9b9f2d162170
Kartals <bkrprs@icloud.com>
2020-12-27 23:51:23 +0100

    ORM-table creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,19 +1,23 @@
 package com.cybertek.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName;
+
+    @Column(name = "studentLastName")
     private String lastName;
+
+    @Column(name = "studentEmailAddress")
     private String email;
 }
