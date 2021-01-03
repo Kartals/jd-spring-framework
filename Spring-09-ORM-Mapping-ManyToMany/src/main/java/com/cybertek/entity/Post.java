6eb6bc84206281bdbe68bc1542e64857eb2c7da0
Kartals <bkrprs@icloud.com>
2021-01-03 23:38:35 +0100

    ORM-Mapping-ManyToMany

--- Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
@@ -25,4 +25,8 @@ public class Post {
     private String title;
     private String description;
 
+    public Post(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
