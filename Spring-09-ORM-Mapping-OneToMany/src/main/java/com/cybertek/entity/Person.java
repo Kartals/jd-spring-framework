809c96e0c8ac133e673f95f1f514c432827d0ed6
Kartals <bkrprs@icloud.com>
2021-01-03 21:27:47 +0100

    ORM-Mapping-OneToMany

---
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -0,0 +1,25 @@
+package com.cybertek.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany (mappedBy = "person") // bunu yaparak person_address table creat edilmiyor.
+    private List<Address> address;
+
+    /*
+    @OneToMany
+    @JoinColumn (name="perso_id")
+    private List<Address> address;
+     */
+
+}
