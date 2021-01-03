a5790e323052a2b725eeea32dd0ff9778fa1aed7
Kartals <bkrprs@icloud.com>
2021-01-03 22:21:52 +0100

    ORM-Mapping-OneToMany

--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/repository/PersonRepository.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/repository/PersonRepository.java
@@ -1,8 +1,10 @@
 package com.cybertek.repository;
 
 
+import com.cybertek.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PersonRepository {
+public interface PersonRepository extends JpaRepository <Person,Long> {
 }
