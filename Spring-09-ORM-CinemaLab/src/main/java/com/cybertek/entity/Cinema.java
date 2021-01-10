e4df630921cb70510dc3684cde71f000e3ff751f
Kartals <bkrprs@icloud.com>
2021-01-10 04:25:54 +0100

    ORM-Lab-CinemaApp

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -15,4 +17,13 @@ public class Cinema extends BaseEntity{
 
     private String name;
     private String sponsoredName;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+    public Cinema(String name, String sponsoredName) {
+        this.name = name;
+        this.sponsoredName = sponsoredName;
+    }
 }
