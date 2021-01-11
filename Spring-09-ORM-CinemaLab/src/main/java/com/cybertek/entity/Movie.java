81bbdcf216e7d9fcab73d1224baf1051f681866b
Kartals <bkrprs@icloud.com>
2021-01-11 22:32:50 +0100

    ORM-Lab-CinemaApp

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
@@ -1,12 +1,9 @@
 package com.cybertek.entity;
-
-
 import com.cybertek.enums.MovieState;
 import com.cybertek.enums.MovieType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
