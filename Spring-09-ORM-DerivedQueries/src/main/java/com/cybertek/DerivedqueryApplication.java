9071ceebeb4de1d37841c6607fa6a108c3e62260
Kartals <bkrprs@icloud.com>
2021-01-18 00:42:13 +0100

    ORM-Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
@@ -1,13 +1,35 @@
 package com.cybertek;
 
+import com.cybertek.entity.Region;
+import com.cybertek.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DerivedqueryApplication {
 
+	@Autowired
+	RegionRepository regionRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DerivedqueryApplication.class, args);
 	}
+
+	@PostConstruct
+	public void testRegions(){
+
+		System.out.println("---------Regions Start------------");
+
+		System.out.println("findByCountry = " + regionRepository.findByCountry("Canada"));
+		System.out.println("findDistinctByCountry = " + regionRepository.findDistinctByCountry("Canada"));
+		System.out.println("findByCountryContaining = " + regionRepository.findByCountryContaining("United"));
+		System.out.println("findByCountryContainingOrderByCountry = " + regionRepository.findByCountryContainingOrderByCountry("United"));
+
+		System.out.println("---------Regions End------------");
+	}
+
 
 }
--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/entity/Region.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/entity/Region.java
@@ -4,6 +4,7 @@ package com.cybertek.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Region extends BaseEntity{
 
     private String region;
